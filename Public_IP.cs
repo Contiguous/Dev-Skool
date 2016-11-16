88faec9b862490338ae80852166ddb2fff2fe381
Robotdandelion <robotdandelion@gmail.com>
2016-11-15 17:14:27 -0700

    Public_IP_Class Unfinished

---
+++ Public_IP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Dev_School
+{
+    class Public_IP
+    { public void Main(WebRequest)
+
+
+    {
+        static string GetIPAddress()
+    {
+        String address = "";
+        WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+        using (WebResponse response = request.GetResponse())
+        using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+        {
+            address = stream.ReadToEnd();
+        }
+
+        //Search for the ip in the html
+        int first = address.IndexOf("Address: ") + 9;
+        int last = address.LastIndexOf("");
+        address = address.Substring(first, last - first);
+
+        return address;
+    }
+}
